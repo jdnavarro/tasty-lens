20366978454a509f529991902fa69586de1d0191
Danny Navarro <j@dannynavarro.net>
2015-05-21 11:54:32 +0200

    Add tests for tuple lens

---
+++ tests/tasty.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Control.Lens
+import Test.Tasty (TestTree, defaultMain, testGroup)
+
+import Test.Tasty.SmallCheck.Lens
+
+main :: IO ()
+main = defaultMain $ testGroup "Lens tests"
+  [ tupleTests
+  ]
+
+tupleTests :: TestTree
+tupleTests = testGroup "Tuple"
+  [ testGroup "_1 :: Lens' (Char,Char) Char"
+    [ testLens (_1 :: Lens' (Char,Char) Char) ]
+  , testGroup "_2 :: Lens' (Char,Char,Char) Char"
+    [ testLens (_2 :: Lens' (Char,Char,Char) Char) ]
+  ]
