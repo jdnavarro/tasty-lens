bf03b63674f9bea4630620ea9642df1c8c53b471
Danny Navarro <j@dannynavarro.net>
2015-05-25 15:39:04 +0200

    Add tests covering `Iso`s and `Prism`s

--- tests/tasty.hs
+++ tests/tasty.hs
@@ -1,23 +1,32 @@
 module Main where
 
 import Control.Lens
+import Numeric.Lens (hex, negated, adding)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
 import Test.Tasty.SmallCheck.Lens
 import Test.Tasty.SmallCheck.Prism
+import Test.Tasty.SmallCheck.Iso
 
 main :: IO ()
 main = defaultMain $ testGroup "Lens tests"
   [ tupleTests
   , maybeTests
+  , numTests
   ]
 
 tupleTests :: TestTree
 tupleTests = testGroup "Tuples"
-  [ testGroup "_1 :: Lens' (Char,Char) Char"
-    [ testLens (_1 :: Lens' (Char,Char) Char) ]
-  , testGroup "_2 :: Lens' (Char,Char,Char) Char"
-    [ testLens (_2 :: Lens' (Char,Char,Char) Char) ]
+  [ testGroup "_1"
+    [ testGroup "Lens' (Char,Char) Char"
+        [ testLens (_1 :: Lens' (Char,Char) Char) ]
+    , testGroup "Lens' (Int,Char) Int"
+        [ testLens (_1 :: Lens' (Int,Char) Int) ]
+    ]
+  , testGroup "_2"
+     [ testGroup "Lens' (Char,Char,Char) Char"
+         [ testLens (_2 :: Lens' (Char,Char,Char) Char) ]
+     ]
   ]
 
 maybeTests :: TestTree
@@ -33,5 +42,27 @@ maybeTests = testGroup "Maybe"
       [ testPrism (_Just :: Prism' (Maybe Char) Char) ]
     , testGroup "Prism' (Maybe Int) Int"
       [ testPrism (_Just :: Prism' (Maybe Int) Int) ]
+    ]
+  ]
+
+numTests :: TestTree
+numTests = testGroup "Numeric"
+  [ testGroup "negated"
+    [ testGroup "Iso' Int Int"
+      [ testIso (negated :: Iso' Int Int) ]
+    , testGroup "Iso' Float Float"
+      [ testIso (negated :: Iso' Float Float) ]
+    ]
+  , testGroup "hex"
+    [ testGroup "Prism'' String Int"
+      [ testPrism (hex :: Prism' String Int) ]
+    , testGroup "Prism'' String Integer"
+      [ testPrism (hex :: Prism' String Integer) ]
+    ]
+  , testGroup "adding"
+    [ testGroup "Iso' Int Int"
+      [ testIso (adding 2 :: Iso' Int Int) ]
+    , testGroup "Iso' Integer Integer"
+      [ testIso (adding 3 :: Iso' Integer Integer) ]
     ]
   ]
