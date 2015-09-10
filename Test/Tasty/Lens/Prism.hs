278afb8089422847ebbdc88b082bb2961694fbea
Danny Navarro <j@dannynavarro.net>
2015-09-10 16:34:30 +0200

    Update `Prism` module to `smallcheck-lens-0.3` API

--- Test/Tasty/Lens/Prism.hs
+++ Test/Tasty/Lens/Prism.hs
@@ -4,14 +4,18 @@
 --
 -- > import qualified Test.Tasty.Lens.Prism as Prism
 --
-module Test.Tasty.Lens.Prism where
+module Test.Tasty.Lens.Prism
+  ( test
+  , module Test.SmallCheck.Lens.Prism
+  ) where
 
+import Data.Proxy (Proxy (..))
 import Control.Lens
 import Test.SmallCheck.Series (Serial(series), CoSerial)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.SmallCheck (testProperty)
 
-import Test.SmallCheck.Lens.Prism
+import Test.SmallCheck.Lens.Prism (yin, yang)
 import qualified Test.Tasty.Lens.Traversal as Traversal
 
 -- | A 'Prism'' is only legal if it is a valid 'Traversal'' (see
@@ -27,9 +31,7 @@ test
      )
   => Prism' s a -> TestTree
 test l = testGroup "Prism Laws"
-  [ Traversal.test l
-  , testProperty "preview l (review l b) ≡ Just b" $
-      prismYin l series
-  , testProperty "maybe s (review l) (preview l s) ≡ s" $
-      prismYang l series
+  [ Traversal.test (Proxy :: Proxy Maybe) l
+  , testProperty "preview l (review l b) ≡ Just b" $ yin l series
+  , testProperty "maybe s (review l) (preview l s) ≡ s" $ yang l series
   ]
